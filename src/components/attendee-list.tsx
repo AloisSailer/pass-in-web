389b54e871f1f0d83dc1edfe3bb2a5379b120fb9
Alois Sailer <alois_sailer@yahoo.com.br>
2024-04-03 23:13:35 -0300

    continuação aula1

--- src/components/attendee-list.tsx
+++ src/components/attendee-list.tsx
@@ -1,3 +1,11 @@
 export function AttendeeList() {
-  return <h1>Participantes</h1>
+  return (
+    <div>
+      <h1 className="text-2xl font-bold">Participantes</h1>
+      <input
+        className="px-3 py-1.5 border border-white/10 bg-transparent rounded-lg text-sm"
+        placeholder="Buscar participante..."
+      />
+    </div>
+  )
 }
